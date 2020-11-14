6d136776ca5526d64bdb558592c168c1524b024d
bhrdj <steven@bhrdwj.net>
2020-11-14 23:52:25 +0800

    MVP prime generator

--- bhrime.py
+++ bhrime.py
@@ -3,4 +3,15 @@
 # python 3.8
 # updated 2020-11-14 by Steven Bhardwaj
 
+def bhrime(n):
+    # github: bhrdj/bhack/bhrime.py
+    # output list of prime numbers from 2 up to and including n
 
+    b = [True]*(n+1)
+    b[:2] = [False]*2    
+    for i in range(2,n):
+        if b[i]:
+            for j in range(2,n//i+1):
+                b[i*j] = False
+                print(i,j,b[i*j])
+    return b
