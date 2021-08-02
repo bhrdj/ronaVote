75a23d33e0ea10991bca6f220202a9f42e929fc0
bhrdj <steven@bhrdwj.net>
2021-08-02 04:42:44 -0500

    got date slider to work

--- ronaVoteShiny.R
+++ ronaVoteShiny.R
@@ -1,11 +1,6 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
+# pathRona <- "https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-counties.csv"
+# pathVote <- "https://raw.githubusercontent.com/bhrdj/ronaVote/main/data/vote/Harvard/countypres_2000-2020.csv"
+# pathPop <- "https://raw.githubusercontent.com/bhrdj/ronaVote/main/data/countyData/USDA_PopulationData/PopEst2019.csv"
 
 # library(rsconnect)
 # rsconnect::deployApp('~/git/RmdSandbox/')
@@ -13,15 +8,16 @@
 
 # https://www.shinyapps.io/admin/#/dashboard
 
+# RUN THESE TO CLEAR LOCAL DATA BUILDING UP IN R WORKSPACE
+# rm(list = ls())
+# .rs.restartR()
+
+
 library(shiny)
 library(scales)
 library(data.table)
 library(broom)
 library(tidyverse)
-
-# pathRona <- "https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-counties.csv"
-# pathVote <- "https://raw.githubusercontent.com/bhrdj/ronaVote/main/data/vote/Harvard/countypres_2000-2020.csv"
-# pathPop <- "https://raw.githubusercontent.com/bhrdj/ronaVote/main/data/countyData/USDA_PopulationData/PopEst2019.csv"
 
 pathRona <- "/home/bhrdwj/git/ronaVote/data/rona/NYT/us-counties_panel_2021-07-17.csv"
 pathVote <- "/home/bhrdwj/git/ronaVote/data/vote/Harvard/countypres_2000-2020.csv"
@@ -36,37 +32,23 @@ vote <- fread(pathVote, select = grep(      "year|party|mode|candidate|county_na
                                             names(fread(pathVote, nrow = 0L)))) %>%
                                             rename(fips = county_fips)
 
-rawData <- list(ronaDays=ronaDays, popu=popu, vote=vote) %>%
+rawData <- list(ronaDays=ronaDays, popu=popu, vote=vote) %>%                    # Clean up the workspace
     map(function(x) { filter(x, !is.na(fips)) })
-
 rm(pathRona, pathVote, pathPop, ronaDays, popu, vote)
 
 zeroMonday <- as.IDate("2019-12-29")                                            # Base date for counting weeks
 zeroMondayInt <- as.numeric(zeroMonday)
 
 ronaTall <- rawData[["ronaDays"]] %>%
-    mutate(weekNum = (as.numeric(date) - as.numeric(zeroMondayInt)) %/% 7) %>%    # Number the weeks from zeroMonday
-    distinct(fips, weekNum, .keep_all = TRUE) %>%                                 # Remove extra rows after first row/week/county
-    mutate(weekDate = as.IDate(weekNum * 7, origin=zeroMonday)) %>%               # Reconstruct uniform dates for all rows in each week
-    mutate(week_DateT = paste("T", gsub("-", "_", weekDate), sep="")) %>%         # for casesDiff row/colnames: with _ instead of -, +prefix "w" for "week number"
-    mutate(week_DateW = paste("W", gsub("-", "_", weekDate), sep="")) %>%         # for casesTot row/colnames: with _ instead of -, +prefix "w" for "week number"
-    mutate(fips2 = paste("x", fips, sep = ""))                                    # for col or row names
+    mutate(weekNum = (as.numeric(date) - as.numeric(zeroMondayInt)) %/% 7) %>%  # Number the weeks from zeroMonday
+    distinct(fips, weekNum, .keep_all = TRUE) %>%                               # Remove extra rows after first row/week/county
+    mutate(weekDate = as.IDate(weekNum * 7, origin=zeroMonday)) %>%             # Reconstruct uniform dates for all rows in each week
+    mutate(week_DateT = paste("T", gsub("-", "_", weekDate), sep="")) %>%       # for casesDiff row/colnames: with _ instead of -, +prefix "w" for "week number"
+    mutate(week_DateW = paste("W", gsub("-", "_", weekDate), sep="")) %>%       # for casesTot row/colnames: with _ instead of -, +prefix "w" for "week number"
+    mutate(fips2 = paste("x", fips, sep = ""))                                  # for col or row names
 
 weekDates <- distinct(ronaTall, week_DateT, week_DateW, weekDate) #THIS IS NEW
 fips_fips2 <- distinct(ronaTall, fips, fips2, .keep_all = FALSE)                # keep fips fips2 dictionary
-
-# TIDY casesTot: WIDEN, NA'S-TO-ZEROES -----------------------------------------
-ronaSectionsTot <- ronaTall  %>%                                                # WIDEN FOR COLUMNS NAMED BY WEEKDATES
-    pivot_wider(id_cols = fips2, 
-                names_from = week_DateT, 
-                values_from = casesTot) %>%                                       # Only 1 row/county; 1 col/week. Total cases at each week
-    mutate(across(starts_with("T"), ~replace_na(., 0)))                           # REPLACE casesTot NA'S WITH ZEROES
-
-ronaSeriesesTot <- ronaTall  %>%                                                # WIDEN FOR COLUMNS NAMED BY WEEKDATES
-    pivot_wider(id_cols = week_DateW, 
-                names_from = fips2, 
-                values_from = casesTot) %>%                                       # Only 1 row/county; 1 col/week. Total cases at each week
-    mutate(across(starts_with("x"), ~replace_na(., 0)))                           # REPLACE casesTot NA'S WITH ZEROES
 
 
 # TIDY vote: WIDEN BY candidate ------------------------------------------------
@@ -83,7 +65,26 @@ vote_wide <- rawData[["vote"]] %>%
               county_name = unique(county_name), 
               totalvotes = unique(totalvotes)) %>%
     mutate(DEM_Margin = (DEMOCRAT - REPUBLICAN) / totalvotes)
-    
+
+
+
+# TIDY casesTot: WIDEN, NA'S-TO-ZEROES -----------------------------------------
+ronaSectionsTot <- ronaTall  %>%                                                # WIDEN rona FOR TOTAL CASES COLUMNS NAMED BY WEEKDATES
+    pivot_wider(id_cols = fips2,                                                # ONE ROW FOR EACH FIPS COUNTY
+                names_from = week_DateT, 
+                values_from = casesTot) %>%                                       # Only 1 row/county; 1 col/week. Total cases at each week
+    mutate(across(starts_with("T"), ~replace_na(., 0)))                           # REPLACE casesTot NA'S WITH ZEROES
+
+
+# TIDY TO TIME SERIES TO CALCULATE WEEKLY NEW CASES FROM ACCUMULATED TOTAL CASES
+ronaSeriesesTot <- ronaTall  %>%                                                # WIDEN TO ONE OBS PER COUNTY, ONE FIELD PER WEEK
+    pivot_wider(id_cols = week_DateW,                                           # ROW F/EA WEEKDATE, COL F/EA FIPS
+                names_from = fips2, 
+                values_from = casesTot) %>%                                       # Only 1 row/county; 1 col/week. Total cases at each week
+    mutate(across(starts_with("x"), ~replace_na(., 0)))                           # REPLACE casesTot NA'S WITH ZEROES
+
+
+
 
 # DIFF SERIES, TRANSPOSE TO SECTIONS, JOIN -------------------------------------
 # DIFF SERIES
@@ -91,7 +92,8 @@ diffit <- function(x) { {{x}} - lag({{x}}) }
 ronaDiff <- ronaSeriesesTot %>%
     mutate(across(starts_with("x"), ~diffit(.) ))
 
-# TRANSPOSE ronaDiff TO SECTIONS, JOIN TO MAKE ronaAllSections
+# TRANSPOSE ronaDiff TO SECTIONS
+# JOIN WITH ronaSectionsTot TO MAKE ronaAllSections
 ronaAllSections <- ronaDiff %>%
     pivot_longer(cols = starts_with("x"), names_to = "fips2") %>%
     pivot_wider(id_cols = fips2, 
@@ -159,7 +161,8 @@ lm_dfT2 <- filter(lm_dfT2, weekDate > as.Date("2020-04-12"))
 lm_dfW2 <- filter(lm_dfW2, weekDate > as.Date("2020-04-12"))
 
 
-# REGRESS
+# PLOT -------------------------------------------------------------------------
+
 
 
 
@@ -172,11 +175,12 @@ ui <- fluidPage(
     # Sidebar with a slider input for number of bins 
     sidebarLayout(
         sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
+            sliderInput("Week",
+                        "Date:",
+                        min = as.Date(weekDates$weekDate[1], "%Y-%m-%d"),
+                        max = as.Date(weekDates$weekDate %>% .[length(.)], "%Y-%m-%d"),
+                        value = as.Date(weekDates$weekDate %>% .[length(.)], "%Y-%m-%d"),
+                        timeFormat = "%Y-%m-%d")
         ),
 
         # Show a plot of the generated distribution
